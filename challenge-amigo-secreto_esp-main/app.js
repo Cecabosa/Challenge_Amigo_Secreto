9c2dc1a793603daf71752c86dbb29334825d629e
Yalile <bonillasaavedrayalileandrea@gmail.com>
2025-02-05 21:36:41 -0500

    Add files via upload
    Se vuelve a subir app.js con comentarios

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,10 +3,10 @@
 var listaAmigos = []; // Arreglo para manejo de la lista de amigos (guardado)
 
 
- function agregarAmigo()
+ function agregarAmigo() // Función que agrega un amigo a la lista de amigos
  {
     let titulo = document.getElementById('titulo-amigos');
-    titulo.style.display = 'inline';
+    titulo.style.display = 'inline';  // Muestra el titulo Listado de amigos que estaba oculto en HTML
     let cajaEntrada = document.getElementById('amigo');
     if(cajaEntrada.value =='')     // Validando si el campo de texto viene vacío
      alert('El texto está vacío. Por favor digite un nombre válido');
@@ -15,44 +15,43 @@ var listaAmigos = []; // Arreglo para manejo de la lista de amigos (guardado)
     creaListaHtml(cajaEntrada.value);   // Llamado a la función que me adiciona dinámicamente un elemento de la lista en la p+agina
     cajaEntrada.value  = '';  // Instrucción para vaciar la caja de texto
     let botonEscoge = document.getElementById('escoge');
-    botonEscoge.disabled = false;
+    botonEscoge.disabled = false;   // Habilita el boton de escoger amigos que venía desabilitado en HTML
  }
 
  function creaListaHtml (nombreAmigo)    // Función que me genera un nuevo de la lista Ul. Recibe como parámetro el nombre del amigo a ingresar
  {
-    let listaHtml = document.getElementById('listaAmigos');
+    let listaHtml = document.getElementById('listaAmigos'); //Crea la lista dinámica de li en html
     var elemento = document.createElement('li');
-    elemento.className ="prueba";
+    elemento.className ="prueba"; //Asigna una clase llamada prueba a cada li para ser usada en el CSS
     elemento.textContent = nombreAmigo;
-    listaHtml.appendChild(elemento);   
+    listaHtml.appendChild(elemento);   //Genera el nombre en la lista
  }
 
- function sortearAmigo()
+ function sortearAmigo() // Función que escoge el amigo secreto. No ingresan parámetros
  {
-   
    let tamano = listaAmigos.length; // encuentro el tamaño de la lista de amigos
    do
    {
-      numeroAleatorio = Math.floor(Math.random()*tamano+1); 
-   }while (numeroAleatorio > tamano && numeroAleatorio < 1)
-   creaListaHtmlResultado (numeroAleatorio -1);
+      numeroAleatorio = Math.floor(Math.random()*tamano+1); // Genera el numero aleatorio de la posición del arreglo de amigos
+   }while (numeroAleatorio > tamano && numeroAleatorio < 1) // Mientras el número generado es manos que 0 o mayor que el tamaño del arreglo -1, vuelve a generar un nuevo número
+   creaListaHtmlResultado (numeroAleatorio -1); // Llama a funcion que imprime el nombre del amigo seleccionado y envía parámetro de posicíon del arreglo
    let botonEscoge = document.getElementById('escoge');
-   botonEscoge.disabled = true;
+   botonEscoge.disabled = true; // Deshabilita botón de escoger
    let botonGrabar = document.getElementById('adicionar');
-   botonGrabar.disabled = true;
+   botonGrabar.disabled = true; // Deshabilita botón de añadir
  }
 
  function creaListaHtmlResultado (posicionArreglo)    // Función que me genera un nuevo de la lista Ul. Recibe como parámetro el nombre del amigo a ingresar
- {
+ {                                                    // ingresa parámetro posiciónArreglo con la posición del arreglo encontrada de manera aleatoria
     let listaHtml = document.getElementById('resultado');
-    var elemento = document.createElement('li');
-    elemento.textContent = 'Mi amigo secreo es: ' + listaAmigos[posicionArreglo];
+    var elemento = document.createElement('li'); //Crea la lista dinámica de li en html
+    elemento.textContent = 'Mi amigo secreo es: ' + listaAmigos[posicionArreglo]; // adiciona el texco con el amigo encontrado en el arreglo
     listaHtml.appendChild(elemento);   
-    var btRepetir = document.getElementById('repite');
-    btRepetir.style.display = "inline";
+    var btRepetir = document.getElementById('repite'); 
+    btRepetir.style.display = "inline"; // Muestra el botón de volver a jugar que estba oculto por HTML
  }
 
- function repetir()
+ function repetir() // Función que reincia página con el juego 
  {
-    location.reload();
+    location.reload(); // recarga página a estado incial
  }
